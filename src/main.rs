81764b6e80c4b58f30646104669db17e00d71231
SurajPatil44 <suraj.patil716@gmail.com>
2024-10-27 21:56:29 +0530

    Bug fixes 	modified:   src/main.rs 	modified:   test_files/test_case_hard.txt 	deleted:    src/.main.rs.swp

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 use std::fs::File;
+use std::io::stdout;
+use std::io::Write;
 use std::io::{self, BufRead};
 use std::{env, path::Path};
 
@@ -60,14 +62,31 @@ impl<const N: usize> SudokuSolver<N> {
         /*if self.matrix[row][col].pos_sols[usize::from(num - 1)] {
             return true;
         }*/
-        let check = (0..N).all(|i| self.matrix[row][i].num != num)
+        /*let check = (0..N).all(|i| self.matrix[row][i].num != num)
             && (0..N).all(|i| self.matrix[i][col].num != num);
+        */
+        let mut check = false;
+        for i in 0..N {
+            if self.matrix[row][i].num == num {
+                check = true;
+            }
+        }
+        for i in 0..N {
+            if self.matrix[i][col].num == num {
+                check = true;
+            }
+        }
         let row = row - row % 3;
         let col = col - col % 3;
-        check
-            && (0..3)
-                .zip(0..3)
-                .all(|(i, j)| self.matrix[row + i][col + j].num != num)
+
+        for i in 0..3 {
+            for j in 0..3 {
+                if self.matrix[row + i][col + j].num == num {
+                    check = true;
+                }
+            }
+        }
+        !check
     }
 
     fn place(&mut self, row: usize, col: usize, num: u8) {
@@ -92,36 +111,33 @@ impl<const N: usize> SudokuSolver<N> {
     }
 
     fn solver(&mut self) -> bool {
+        let mut break_condition = true;
         let mut checking_range = BinaryHeap::new();
-        for iind in 0..9 {
-            for jind in 0..9 {
-                if self.matrix[iind][jind].num == 0 {
+
+        for i in 0..N {
+            for j in 0..N {
+                if self.matrix[i][j].num == 0 {
                     let mut pos_sols: usize = 0;
-                    for i in 1..10 {
-                        if self.check_matrix(iind, jind, i as u8) {
+                    break_condition = false;
+                    for num in 1..N + 1 {
+                        if self.check_matrix(i, j, num as u8) {
                             pos_sols += 1;
-                            self.matrix[iind][jind].pos_sols[i - 1] = true;
+                            self.matrix[i][j].pos_sols[num - 1] = true;
                         }
                     }
-                    /*
-                    let pos_sols = self.matrix[iind][jind]
-                        .pos_sols
-                        .iter()
-                        .filter(|&n| *n)
-                        .count();
-                    */
-                    checking_range.push(Reverse((pos_sols, iind, jind)));
+                    checking_range.push(Reverse((pos_sols, i, j)));
                 }
             }
         }
-        let first = checking_range.pop();
-        if first.is_none() {
+
+        if break_condition {
+            println!("sudoku is solved");
             return true;
         }
-        let minimum_loc: (usize, usize) = (first.unwrap().0 .1, first.unwrap().0 .2);
-        //let low = first.0;
 
-        for i in 1..10 {
+        let first = checking_range.pop();
+        let minimum_loc: (usize, usize) = (first.unwrap().0 .1, first.unwrap().0 .2);
+        for i in 0..10 {
             if self.check_matrix(minimum_loc.0, minimum_loc.1, i as u8) {
                 self.place(minimum_loc.0, minimum_loc.1, i as u8);
                 if self.solver() {
@@ -131,8 +147,10 @@ impl<const N: usize> SudokuSolver<N> {
                 self.place(minimum_loc.0, minimum_loc.1, 0 as u8);
             }
         }
-
-        false
+        print!("\r\r\r\r\r");
+        println!("{}", self.print_matrix());
+        let _ = stdout().flush();
+        return false;
     }
 
     fn solve(&mut self) -> bool {
@@ -140,29 +158,33 @@ impl<const N: usize> SudokuSolver<N> {
         self.solver()
     }
 
-    fn print_matrix(&self) {
+    fn print_matrix(&self) -> String {
         let mut i = 0;
+        let mut fmt = String::new();
         for row in &self.matrix[..] {
             i += 1;
             if i % 3 == 1 && i > 3 {
-                println!("-----------------------");
+                //fmt.push_str("-----------------------");
+                fmt.push('\n')
             }
             for (j, tile) in row.iter().enumerate() {
                 if j % 3 == 0 && j > 0 {
-                    print!(" | ");
+                    fmt.push_str("║ ║");
                 }
                 match tile.num {
-                    0 => print!("_"),
-                    n => print!("{}", n),
+                    0 => fmt.push_str(" - "),
+                    n => fmt.push_str(&format!(" {} ", n)),
                 }
                 if j % 3 < 2 {
-                    print!("|");
+                    fmt.push_str("┃");
                 } else {
-                    print!("");
+                    //fmt.push('');
                 }
             }
-            println!();
+            fmt.push_str("\n━━━┛━━━┛━━━┛ ┗━━━┛━━━┛━━━┛ ┗━━━┛━━━┛━━━\n");
         }
+        fmt.push_str("============================\n");
+        fmt
     }
 }
 
@@ -170,9 +192,9 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let solver = SudokuSolver::<9>::default();
     let mut solver = solver.read_files(&args[1]);
-    solver.print_matrix();
+    println!("{}", solver.print_matrix());
     if solver.solve() {
         println!("took {} calls", solver.num_calls);
-        solver.print_matrix();
+        println!("{}", solver.print_matrix());
     }
 }
